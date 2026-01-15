33d92be6edec67fd2f181d333967aef0d73b71da
LuisAlar <alarcon.mandujano.luis@gmail.com>
2026-01-15 12:46:34 -0600

    Adds nav bar for layouts. Dynamically switches through different layouts. TODO: addNew layout feature on nav. Design layouts for about and Projects

---
+++ yourOwn/apps/client/src/Features/portfolio-preview/types/portoflio.types.ts
@@ -0,0 +1,31 @@
+export type Tab = {
+  id: string;
+  name: string;
+};
+
+export type ManifestData = {
+  tabs: Tab[];
+  sessionId: string;
+};
+
+export type Placement = {
+  slotId: string;
+  experienceId: string;
+  patternId: string;
+};
+
+export type HydratedLayoutData = {
+  id: string;
+  layoutName: string;
+  slots: Array<{
+    id: string;
+    area: string;
+  }>;
+  placements: Placement[];
+  experiences: Array<{
+    id: string;
+    title: string;
+    description?: string;
+    kind: string;
+  }>;
+};
